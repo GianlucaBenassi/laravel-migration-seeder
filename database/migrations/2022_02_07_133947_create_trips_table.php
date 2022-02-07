7a5a46c6b34556b24587635375624b89c3dce24b
Gianluca Benassi <benox1199@gmail.com>
2022-02-07 14:58:10 +0100

    migration create trips table

---
+++ database/migrations/2022_02_07_133947_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->unsignedTinyInteger('places');
+            $table->boolean('fly')->default(false);
+            $table->float('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
